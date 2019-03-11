a91f4feb6b4601d00098060100b2fd854e53a156
feras-oughali <oughali.feras@gmail.com>
2019-03-11 07:02:15 +0300

    added step 13

--- pandas_cub/__init__.py
+++ pandas_cub/__init__.py
@@ -251,12 +251,11 @@ class DataFrame:
             return DataFrame({i:self._data[i] for i in item})
         if isinstance(item, DataFrame):
             if len(item.columns) != 1: raise ValueError('DataFrame must have one col only')
-            arr = next(iter(list(item.values())))
-            if arr.dtype.kind !='b': raise TypeError('DataFrame must be of bool type')
-            cols = list(self._data.keys())
-            data = list(self._data.values())
-            
-            return DataFrame({i:self._data[i][arr] for i in cols})
+            b_arr = next(iter(list(item._data.values())))
+            if b_arr.dtype.kind !='b': raise TypeError('DataFrame must be of bool type')
+            return DataFrame({col:val[b_arr] for col,val in self._data.items()})
+
+
 
     def _getitem_tuple(self, item):
         # simultaneous selection of rows and cols -> df[rs, cs]
